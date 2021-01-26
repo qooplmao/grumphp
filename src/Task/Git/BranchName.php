1044cfac4ef4a320f2eb422570eb7c3a4f41854c
Michael Fitzpatrick <fitz@michaelfitzpatrick.co.uk>
2021-01-26 09:20:50 +0000

    add message for whitelist match when blacklisted

--- src/Task/Git/BranchName.php
+++ src/Task/Git/BranchName.php
@@ -107,6 +107,8 @@ class BranchName implements TaskInterface
             
             if (preg_match((string) $regex, $name)) {
                 if ($isBlacklisted) {
+                    $errors[] = sprintf('Matched whitelist rule (IGNORED due to presence in blacklist): %s', $rule);
+                    
                     continue;
                 }
                 
